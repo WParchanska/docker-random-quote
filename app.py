77a25ad9feff9292da0a7b946ebf196dd013e988
WParchanska <parchanskaw@gmail.com>
2025-10-08 12:44:43 +0200

    Random Quote Docker project

---
+++ app.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Keep it simple!",
+    "Docker is awesome!",
+    "Learning every day.",
+    "Python + Docker = fun!"
+]
+
+print("Your quote for today:", random.choice(quotes))
+
